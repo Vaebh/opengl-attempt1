29b063d9366a626c90272bc4416f8a06cd4aeb27
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-01-20 11:24:09 +0000

    First implementation of glfw code.

--- trunk/OpenGL Attempt1/OpenGL Attempt1/main.cpp
+++ trunk/OpenGL Attempt1/OpenGL Attempt1/main.cpp
@@ -1,11 +1,37 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello OpenGL" << endl;
-	cin.get();
+  glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit())
+    exit(EXIT_FAILURE);
+    
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Red Square Adventures", NULL, NULL);
+  
+  if (!window)
+  {
+      glfwTerminate();
+      exit(EXIT_FAILURE);
+  }
+  
+  glfwMakeContextCurrent(window);
+  
+  while (!glfwWindowShouldClose(window))
+  {
+      // Keep running
+  }
+  
+  glfwDestroyWindow(window);
+  
+  glfwTerminate();
 
 	return 0;
+}
+
+void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
 }
