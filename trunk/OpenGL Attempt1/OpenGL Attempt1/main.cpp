8edc4b051b609d6962f16011de24d1250cb8e260
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-01-21 00:18:15 +0000

    Fuck yeah, a square that moves with WASD!

--- trunk/OpenGL Attempt1/OpenGL Attempt1/main.cpp
+++ trunk/OpenGL Attempt1/OpenGL Attempt1/main.cpp
@@ -1,37 +1,121 @@
-#include <GLFW/glfw3.h>
+#include <GLFW\glfw3.h>
 
 using namespace std;
 
-int main()
+float moveHorz, moveVert = 0;
+
+double mouseX, mouseY = 0;
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-  glfwSetErrorCallback(error_callback);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 
-	if (!glfwInit())
-    exit(EXIT_FAILURE);
-    
-  GLFWwindow* window = glfwCreateWindow(640, 480, "Red Square Adventures", NULL, NULL);
-  
-  if (!window)
-  {
-      glfwTerminate();
-      exit(EXIT_FAILURE);
-  }
-  
-  glfwMakeContextCurrent(window);
-  
-  while (!glfwWindowShouldClose(window))
-  {
-      // Keep running
-  }
-  
-  glfwDestroyWindow(window);
-  
-  glfwTerminate();
-
-	return 0;
+	if(key == GLFW_KEY_D && action == GLFW_REPEAT || action == GLFW_PRESS)
+		moveHorz += 0.01f;
+	if(key == GLFW_KEY_A && action == GLFW_REPEAT || action == GLFW_PRESS)
+		moveHorz -= 0.01f;
+	if(key == GLFW_KEY_W && action == GLFW_REPEAT || action == GLFW_PRESS)
+		moveVert += 0.01f;
+	if(key == GLFW_KEY_S && action == GLFW_REPEAT || action == GLFW_PRESS)
+		moveVert -= 0.01f;
 }
 
-void error_callback(int error, const char* description)
+void DrawSquare(double centerX, double centerY, float length)
 {
-    fputs(description, stderr);
+	glBegin(GL_QUADS);
+	glColor3f(1.f, 0.f, 0.f);
+	glVertex3f(centerX - length + moveHorz, centerY - length + moveVert, 0.f);
+	glColor3f(0.f, 1.f, 0.f);
+	glVertex3f(centerX + length + moveHorz, centerY - length + moveVert, 0.f);
+	glColor3f(0.f, 0.f, 1.f);
+	glVertex3f(centerX + length + moveHorz, centerY + length + moveVert, 0.f);
+	glColor3f(1.f, 1.f, 1.f);
+	glVertex3f(centerX - length + moveHorz, centerY + length + moveVert, 0.f);
+    glEnd();
+}
+
+static void mouseCallback(GLFWwindow * window, int button, int action, int mods)
+{
+	if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+	{
+		glfwGetCursorPos(window, &mouseX, &mouseY);
+		DrawSquare(0, 0, 0.5f);
+	}
+}
+
+static void mousePosCallback(GLFWwindow * window, double x, double y)
+{
+
+}
+
+
+
+int main(void)
+{
+	if (!glfwInit())
+		return 1;
+    
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Red Square Adventures", NULL, NULL);
+  
+	if (!window)
+	{
+		glfwTerminate();
+			return 1;
+	}
+  
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetMouseButtonCallback(window, mouseCallback);
+
+	glClearColor(0, 0, 0, 1);
+  
+	while (!glfwWindowShouldClose(window))
+	{
+		float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+
+        glMatrixMode(GL_MODELVIEW);
+
+        glLoadIdentity();
+
+		DrawSquare(0, 0, 0.5f);
+
+        //glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+		/*glBegin(GL_QUADS);
+		glColor3f(1.f, 0.f, 0.f);
+		glVertex3f(-0.5f + moveHorz, -0.5f + moveVert, 0.f);
+		glColor3f(0.f, 1.f, 0.f);
+		glVertex3f(0.5f + moveHorz, -0.5f + moveVert, 0.f);
+		glColor3f(0.f, 0.f, 1.f);
+		glVertex3f(0.5f + moveHorz, 0.5f + moveVert, 0.f);
+		glColor3f(1.f, 1.f, 1.f);
+		glVertex3f(-0.5f + moveHorz, 0.5f + moveVert, 0.f);
+        /*glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();*/
+		glfwPollEvents();
+
+        glfwSwapBuffers(window);
+	}
+  
+	glfwDestroyWindow(window);
+  
+	glfwTerminate();
+
+	return 0;
 }
