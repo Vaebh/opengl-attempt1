9646d20c8912ce15e4e35eddd3d57fa390caac09
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-01-26 23:18:54 +0000

    Switched to using shaders, which is apparently the new opengl way of doing things, with the way I was doing it the old no longer used way. Followed open.gl and made a multicoloured and pulsing triangle using a vertex and fragment shader

--- trunk/OpenGL Attempt1/OpenGL Attempt1/main.cpp
+++ trunk/OpenGL Attempt1/OpenGL Attempt1/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
 using namespace std;
@@ -60,26 +62,30 @@ static void mouseCallback(GLFWwindow * window, int button, int action, int mods)
 		// convert mouse coords to range 0 to 1
 		glfwGetCursorPos(window, &mouseX, &mouseY);
 		cout << "XPos: " << mouseX << " YPos: " << mouseY << endl;
-		float thing = (height / (float)width);
-		mouseX = ((mouseX * 2.666f) / 640) - 1.33f;
-		mouseY = ((mouseY * 2.f) / 480) - 1.f;
+
+		float screenRatioW = (width / (float)height);
+		float screenRatioH = (height / (float)width) * screenRatioW;
+
+		mouseX = ((mouseX * (screenRatioW * 2)) / width) - screenRatioW;
+		mouseY = ((mouseY * (screenRatioH * 2)) / height) - screenRatioH;
+
 		cout << "Scaled XPos: " << mouseX << " Scaled YPos: " << mouseY << endl;
+
 		DrawSquare(mouseX, -mouseY, 0.5f, window);
 	}
 }
-
-static void mousePosCallback(GLFWwindow * window, double x, double y)
-{
-
-}
-
-
 
 int main(void)
 {
 	if (!glfwInit())
 		return 1;
     
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Red Square Adventures", NULL, NULL);
   
 	if (!window)
@@ -90,52 +96,134 @@ int main(void)
   
 	glfwMakeContextCurrent(window);
 
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+
+	printf("%u\n", vertexBuffer);
+
+	float vertices[] = 
+	{
+		0.0f,  0.5f, 1.0f, 0.0f, 0.0f, // Vertex 1 (X, Y) red
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // Vertex 2 (X, Y) green
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f  // Vertex 3 (X, Y) blue
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo); // Generate 1 buffer
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Vertex Shader
+	const GLchar * vertexSource = "#version 150\n"
+		"in vec2 position;"
+		"in vec3 color;"
+		"out vec3 Color;"
+		"void main()"
+		"{"
+		"Color = color;"
+		"gl_Position = vec4(position, 0.0, 1.0);"
+		"}";
+
+	cout << vertexSource << endl;
+
+	// Fragment Shader
+	const GLchar * fragmentSource = "#version 150\n"
+		"in vec3 Color;"
+		"out vec4 outColor;"
+		"void main()"
+		"{"
+		"    outColor = vec4(Color, 1.0);"
+		"}";
+
+	// Loading Vertex Shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+
+	glCompileShader(vertexShader);
+
+	// Checking if Vertex Shader compiled or not
+	char buffer[512];
+	glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
+
+	GLint status;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+	if(status == GL_TRUE)
+		cout << "Vertex shader compilation SUCCESS" << endl;
+	else if(status == GL_FALSE)
+		cout << "Vertex shader compilation FAILED" << endl;
+
+	// Loading Fragment Shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+	if(status == GL_TRUE)
+		cout << "Fragment shader compilation SUCCESS" << endl;
+	else if(status == GL_FALSE)
+		cout << "Fragment shader compilation FAILED" << endl;
+
+	// Actually create the shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	// Telling the program which buffer the fragment shader is writing to
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+
+	// Link the program (?)
+	glLinkProgram(shaderProgram);
+
+	// Set our shader to be the active shader
+	glUseProgram(shaderProgram);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	/*GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	cout << posAttrib << endl;
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(posAttrib);*/
+
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,
+						   5*sizeof(float), 0);
+
+	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+	cout << colAttrib << endl;
+	glEnableVertexAttribArray(colAttrib);
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE,
+						   5*sizeof(float), (void*)(2*sizeof(float)));
+
+	// Setting input callbacks
 	glfwSetKeyCallback(window, key_callback);
 	glfwSetMouseButtonCallback(window, mouseCallback);
 
+	// Setting the screen to be black and swapping the buffers
 	glClearColor(0, 0, 0, 1);
 	glfwSwapBuffers(window);
-	
-	glfwGetFramebufferSize(window, &width, &height);
-	ratio = width / (float) height;
-  
+
+	// Loop until the window should close
 	while (!glfwWindowShouldClose(window))
 	{
-		/*float ratio;
-		int width, height;
-		glfwGetFramebufferSize(window, &width, &height);
-		ratio = width / (float) height;
-		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
+		//GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+		//glUniform3f(uniColor, 1.0f, 0.0f, 0.0f);
 
-		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		//float time = (float)clock() / (float)CLOCKS_PER_SEC;
+		//glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
 
-		glMatrixMode(GL_MODELVIEW);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		glLoadIdentity();*/
+		glfwSwapBuffers(window);
 
-		//DrawSquare(0, 0, 0.5f, window);
-
-		//glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-		/*glBegin(GL_QUADS);
-		glColor3f(1.f, 0.f, 0.f);
-		glVertex3f(-0.5f + moveHorz, -0.5f + moveVert, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
-		glVertex3f(0.5f + moveHorz, -0.5f + moveVert, 0.f);
-		glColor3f(0.f, 0.f, 1.f);
-		glVertex3f(0.5f + moveHorz, 0.5f + moveVert, 0.f);
-		glColor3f(1.f, 1.f, 1.f);
-		glVertex3f(-0.5f + moveHorz, 0.5f + moveVert, 0.f);
-		/*glBegin(GL_TRIANGLES);
-		glColor3f(1.f, 0.f, 0.f);
-		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
-		glVertex3f(0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 0.f, 1.f);
-		glVertex3f(0.f, 0.6f, 0.f);
-		glEnd();*/
+		// Check for new events
 		glfwPollEvents();
 	}
 
