7187c8bcb995bcbd9708cace6f2ea02618d872ba
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-02-22 17:01:52 +0000

    GLIncludes.h

---
+++ OpenGL Attempt1/OpenGL Attempt1/GLIncludes.h
@@ -0,0 +1,9 @@
+#ifndef GL_INCLUDES
+#define GL_INCLUDES
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <SOIL.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#endif
