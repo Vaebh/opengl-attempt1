9a4da8b4752c0f67f459e6ac27b816ee90d64173
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-02-22 16:36:15 +0000

    Getting the shader file operational and starting conversion to using it

--- OpenGL Attempt1/OpenGL Attempt1/Shader.cpp
+++ OpenGL Attempt1/OpenGL Attempt1/Shader.cpp
@@ -1,31 +1,36 @@
 #include "Shader.h"
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
 
 Shader::Shader()
 {
   
 }
 
-Shader::Shader(const std::string vertexShaderSrc, const std::string fragShaderSrc)
+Shader::Shader(const std::string& vertexShaderSrc, const std::string& fragShaderSrc)
 {
-        CreateShaderProgram(vertexShaderSrc, fragShaderSrc);
+	CreateShaderProgram(vertexShaderSrc, fragShaderSrc);
 }
 
 Shader::~Shader()
 {
-        if(mVertexShader)
-        {
-                glDetachShader(mShaderProgram, mVertexShader);
-                glDeleteShader(mVertexShader);
-        }
-        if(mFragmentShader)
-        {
-                glDetachShader(mShaderProgram, mFragmentShader);
-                glDeleteShader(mFragmentShader);
-        }
-        if(mShaderProgram)
-        {
-                glDeleteProgram(mShaderProgram);
-        }
+	if(mVertexShader)
+	{
+		glDetachShader(mShaderProgram, mVertexShader);
+		glDeleteShader(mVertexShader);
+	}
+	if(mFragmentShader)
+	{
+		glDetachShader(mShaderProgram, mFragmentShader);
+		glDeleteShader(mFragmentShader);
+	}
+	if(mShaderProgram)
+	{
+		glDeleteProgram(mShaderProgram);
+	}
 }
 
 //-------------------------------------------------------------------------------------
@@ -36,89 +41,89 @@ Shader::~Shader()
 //
 // @return - The index for the shader program we've just created
 //-------------------------------------------------------------------------------------
-GLuint CreateShaderProgram(std::string vertexShaderSrc, std::string fragShaderSrc)
+GLuint Shader::CreateShaderProgram(const std::string& vertexShaderSrc, const std::string& fragShaderSrc)
 {
-        GLuint vertexShader = CreateShaderFromFile(vertexShaderSrc, GL_VERTEX_SHADER);
-        GLuint fragShader = CreateShaderFromFile(fragShaderSrc, GL_FRAGMENT_SHADER);
+	GLuint vertexShader = CreateShaderFromFile(vertexShaderSrc, GL_VERTEX_SHADER);
+	GLuint fragShader = CreateShaderFromFile(fragShaderSrc, GL_FRAGMENT_SHADER);
 
-        if(!ShaderCompilationCheck(vertexShader, "Vertex") || !ShaderCompilationCheck(fragShader, "Fragment"))
-        {
-                return 0;
-        }
+	if(!ShaderCompilationCheck(vertexShader, fragShader, "NAME"))
+	{
+		return 0;
+	}
         
-        mVertexShader = vertexShader;
-        mFragmentShader = fragShader;
+	mVertexShader = vertexShader;
+	mFragmentShader = fragShader;
 
-        // Actually create the shader program
-        GLuint shaderProgram = glCreateProgram();
-        glAttachShader(shaderProgram, vertexShader);
-        glAttachShader(shaderProgram, fragShader);
+	// Actually create the shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragShader);
 
-        // Telling the program which buffer the fragment shader is writing to
-        glBindFragDataLocation(shaderProgram, 0, "outColor");
-        glLinkProgram(shaderProgram);
+	// Telling the program which buffer the fragment shader is writing to
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
         
-        mShaderProgram = shaderProgram;
+	mShaderProgram = shaderProgram;
 
-        return shaderProgram;
+	return shaderProgram;
 }
 
-bool Shader::ShaderCompilationCheck(GLuint vertexShader, GLuint fragmentShader, std::string shaderName)
+bool Shader::ShaderCompilationCheck(const GLuint vertexShader, const GLuint fragmentShader, const std::string shaderName) const
 {
-        GLint vertexStatus, fragmentStatus;
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexStatus);
-        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentStatus);
+	GLint vertexStatus, fragmentStatus;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexStatus);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentStatus);
         
-        if(vertexStatus == GL_TRUE && fragmentStatus == GL_TRUE)
-        {
-                cout << shaderName + " shader compilation SUCCESS" << endl;
-                return true;
-        }
-        else
-        {
-                if(vertexStatus == GL_FALSE)
-                {
-                        cout << "Vertex shader compilation FAILED" << endl;
-                }
-                if(fragmentStatus == GL_FALSE)
-                {
-                        cout << "Fragment shader compilation FAILED" << endl;
-                }
+	if(vertexStatus == GL_TRUE && fragmentStatus == GL_TRUE)
+	{
+		cout << shaderName + " shader compilation SUCCESS" << endl;
+		return true;
+	}
+	else
+	{
+		if(vertexStatus == GL_FALSE)
+		{
+			cout << "Vertex shader compilation FAILED" << endl;
+		}
+		if(fragmentStatus == GL_FALSE)
+		{
+			cout << "Fragment shader compilation FAILED" << endl;
+		}
                 
-                return false;
-        }
+		return false;
+	}
 }
 
-GLuint Shader::CreateShaderFromFile(std::string path, GLenum shaderType)
+GLuint Shader::CreateShaderFromFile(const std::string& path, const GLenum& shaderType)
 {
-        // Get the shader
-        std::string shaderSrcString = LoadShader(path);
+	// Get the shader
+	std::string shaderSrcString = LoadShaderFromFile(path);
 
-        if(shaderSrcString.empty())
-        {
-                return 0;
-        }
+	if(shaderSrcString.empty())
+	{
+		return 0;
+	}
 
-        const GLchar* shaderSrc = shaderSrcString.c_str();
+	const GLchar* shaderSrc = shaderSrcString.c_str();
 
-        // Loading Vertex Shader
-        GLuint shader = glCreateShader(shaderType);
-        glShaderSource(shader, 1, &shaderSrc, NULL);
-        glCompileShader(shader);
+	// Loading Vertex Shader
+	GLuint shader = glCreateShader(shaderType);
+	glShaderSource(shader, 1, &shaderSrc, NULL);
+	glCompileShader(shader);
 
-        return shader;
+	return shader;
 }
 
-std::string Shader::LoadShader(std::string path)
+std::string Shader::LoadShaderFromFile(const std::string& path) const
 {
-        std::string shaderSrc = "";
-        std::ifstream myFile;
+	std::string shaderSrc = "";
+	std::ifstream myFile;
 
-        myFile.open(path);
-        if(myFile.is_open() && !myFile.bad())
-        {
-                return shaderSrc.assign(std::istreambuf_iterator<char>(myFile), std::istreambuf_iterator<char>());
-        }
+	myFile.open(path);
+	if(myFile.is_open() && !myFile.bad())
+	{
+		return shaderSrc.assign(std::istreambuf_iterator<char>(myFile), std::istreambuf_iterator<char>());
+	}
 
-        return "";
+	return "";
 }
--- OpenGL Attempt1/OpenGL Attempt1/Shader.h
+++ OpenGL Attempt1/OpenGL Attempt1/Shader.h
@@ -1,25 +1,27 @@
-class Shader : public
+#include "GLIncludes.h"
+#include <string>
+
+class Shader
 {
 public:
 
-        Shader();
-        Shader(const std::string& vertexShaderSrc, const std::string& fragShaderSrc);
-        ~Shader();
+	Shader();
+	Shader(const std::string& vertexShaderSrc, const std::string& fragShaderSrc);
+	~Shader();
         
-        GLuint GetProgramID();
-        
-private:
-        
-        GLuint CreateShaderProgram(const std::string& vertexShaderSrc, const std::string& fragShaderSrc)
-        GLuint Shader::CreateShaderFromFile(const std::string& path, const GLenum& shaderType);
-        std::string Shader::LoadShaderFromFile(const std::string& path) const;
-        
-        ShaderCompilationCheck(const GLuint& vertexShader, const GLuint& fragmentShader) const;
+	inline GLuint GetProgramID() {return mShaderProgram;}
         
 private:
         
-        GLuint mShaderProgram;
-        GLuint mVertexShader;
-        GLuint mFragmentShader;
-
+	GLuint CreateShaderProgram(const std::string& vertexShaderSrc, const std::string& fragShaderSrc);
+	GLuint CreateShaderFromFile(const std::string& path, const GLenum& shaderType);
+	std::string LoadShaderFromFile(const std::string& path) const;
+        
+	bool ShaderCompilationCheck(const GLuint vertexShader, const GLuint fragmentShader, const std::string shaderName) const;
+        
+private:
+        
+	GLuint mShaderProgram;
+	GLuint mVertexShader;
+	GLuint mFragmentShader;
 };
