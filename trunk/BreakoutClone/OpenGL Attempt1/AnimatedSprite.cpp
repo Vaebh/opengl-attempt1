8951d373f6191ce65feef5c0efbb5c4d82864638
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-06-02 13:55:04 +0000

    Created AnimatedSprite.cpp

---
+++ trunk/BreakoutClone/OpenGL Attempt1/AnimatedSprite.cpp
@@ -0,0 +1,35 @@
+#include "AnimatedSprite.h"
+
+	AnimatedSprite::AnimatedSprite(const std::string inTexture, int inNumFrames, float inAnimationSpeed) : Sprite(inTexture), mAnimSpeed(inAnimationSpeed), mNumFrames(inNumFrames), mCurrentFrame(0)
+   {
+     SendAnimInfo();
+   }
+   
+   void AnimatedSprite::ChangeFrame()
+   {
+        if(mCurrentFrame < mNumFrames - 1)
+          mCurrentFrame += 1;
+        else
+          mCurrentFrame = 0;
+          
+          SendAnimInfo();
+   }
+   
+   void AnimatedSprite::SendAnimInfo()
+   {
+         float spriteFrameDivisorX = 1.f / mNumFrames;
+        
+        glUniform2f(mSpriteWidth, spriteFrameDivisorX, 1.f);
+        glUniform1i(mUniformCurrentFrame, mCurrentFrame);
+   }
+   
+   void AnimatedSprite::Update(float inDT)
+   {
+     mAnimTimer += inDT;
+     
+     if(mAnimTimer >= mAnimSpeed)
+     {
+     mAnimTimer = 0;
+       ChangeFrame();
+     }
+   }
