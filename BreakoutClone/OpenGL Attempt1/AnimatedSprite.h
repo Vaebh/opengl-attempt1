f3705985428275409b473bbd96592885d25cd717
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-06-03 09:20:10 +0000

    Added SendAnimInfo function prototype

--- BreakoutClone/OpenGL Attempt1/AnimatedSprite.h
+++ BreakoutClone/OpenGL Attempt1/AnimatedSprite.h
@@ -4,16 +4,17 @@ class AnimatedSprite : public Sprite
 {
 public:
 	AnimatedSprite(const std::string inTexture, int inNumFrames = 1, float inAnimationSpeed = 1.f);
-   virtual void Update(float inDT);
+  virtual void Update(float inDT);
    
-   private:
-     void ChangeFrame();
+private:
+  void SendAnimInfo();
+  void ChangeFrame();
    
-   private:
-    int mNumFrames;
-    int mCurrentFrame;
+private:
+  int mNumFrames;
+  int mCurrentFrame;
 
-    float mAnimTimer;
-    GLuint mSpriteWidth;
-    GLuint mUniformCurrentFrame;
+  float mAnimTimer;
+  GLuint mSpriteWidth;
+  GLuint mUniformCurrentFrame;
 };
