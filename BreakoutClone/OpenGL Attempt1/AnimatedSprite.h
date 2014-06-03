aeb072de21926db77cadea1bf9bf469bcc66fd61
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-06-03 16:27:58 +0000

    Spacing adjustments

--- BreakoutClone/OpenGL Attempt1/AnimatedSprite.h
+++ BreakoutClone/OpenGL Attempt1/AnimatedSprite.h
@@ -4,17 +4,17 @@ class AnimatedSprite : public Sprite
 {
 public:
 	AnimatedSprite(const std::string inTexture, int inNumFrames = 1, float inAnimationSpeed = 1.f);
-  virtual void Update(float inDT);
+	virtual void Update(float inDT);
    
 private:
-  void SendAnimInfo();
-  void ChangeFrame();
+	void SendAnimInfo();
+	void ChangeFrame();
    
 private:
-  int mNumFrames;
-  int mCurrentFrame;
+	int mNumFrames;
+	int mCurrentFrame;
 
-  float mAnimTimer;
-  GLuint mSpriteWidth;
-  GLuint mUniformCurrentFrame;
+	float mAnimTimer;
+	GLuint mSpriteWidth;
+	GLuint mUniformCurrentFrame;
 };
