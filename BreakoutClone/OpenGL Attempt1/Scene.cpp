98f32fd82bb12c90ac727d67e06769331518c0fb
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-07-04 14:49:04 +0000

    Small changes

--- BreakoutClone/OpenGL Attempt1/Scene.cpp
+++ BreakoutClone/OpenGL Attempt1/Scene.cpp
@@ -1,65 +1,32 @@
 #include "Scene.h"
 
-// TODO - PROBABLY GET RID OF THIS< BUT COMMENT IT OUT FOR NOW
+// TODO - PROBABLY GET RID OF THIS, BUT COMMENT IT OUT FOR NOW
 
 Scene::Scene()
 {
 
 }
 
-bool Scene::IsColliding(Rectangle inBoundingBox, Entity* inEntity)
+void Scene::AddToScene(GameObject* inGameObject)
 {
-	for each(Entity* theEntity in mEntities)
+	if(inGameObject)
 	{
-		if(theEntity != inEntity && theEntity->mCollidable && IsIntersecting(inBoundingBox, theEntity->mBoundingBox))
+		mGameObjects.push_back(inGameObject);
+		inGameObject->SetOwningScene(const_cast<Scene*>(this));
+	}
+}
+
+void Scene::RemoveFromScene(inGameObject* ininGameObject)
+{
+	if(inGameObject)
+	{
+		std::vector<inGameObject*>::const_iterator iter;
+
+		for(iter = mGameObjects.begin(); iter != mGameObjects.end(); ++iter)
 		{
-			Vector3 entity1Center = Vector3(inEntity->GetPosition().x + inEntity->mScale.x / 2, inEntity->GetPosition().y + inEntity->mScale.y / 2, 0.f);
-			Vector3 entity2Center = Vector3(theEntity->GetPosition().x + theEntity->mScale.x / 2, theEntity->GetPosition().y + theEntity->mScale.y / 2, 0.f);
-
-			Vector3 collisionVector = inEntity->GetPosition() - theEntity->GetPosition();//entity1Center - entity2Center;
-
-			//collisionVector *= 4;
-
-			theEntity->OnCollision(inEntity, collisionVector, theEntity->mBoundingBox);
-			inEntity->OnCollision(theEntity, collisionVector, inBoundingBox);
-			return true;
-		}
-	}
-
-	return false;
-}
-
-bool Scene::IsIntersecting(Rectangle inA, Rectangle inB)
-{
-	if(inA.right > inB.left && inA.left < inB.right &&
-		inA.bottom < inB.top && inA.top > inB.bottom)
-	{
-		return true;
-	}
-
-	return false;
-}
-
-void Scene::AddToScene(Entity* inEntity)
-{
-	if(inEntity)
-	{
-		mEntities.push_back(inEntity);
-		inEntity->SetOwningScene(const_cast<Scene*>(this));
-	}
-}
-
-void Scene::RemoveFromScene(Entity* inEntity)
-{
-	if(inEntity)
-	{
-		std::vector<Entity*>::const_iterator iter;
-
-		for(iter = mEntities.begin(); iter != mEntities.end(); ++iter)
-		{
-			if(*iter == inEntity)
+			if(*iter == inGameObject)
 			{
-				mEntities.erase(iter);
+				mGameObjects.erase(iter);
 				break;
 			}
 		}
@@ -68,8 +35,8 @@ void Scene::RemoveFromScene(Entity* inEntity)
 
 void Scene::Update(float inDT)
 {
-	for each(Entity* theEntity in mEntities)
+	for each(GameObject* theGameObject in mGameObjects)
 	{
-		theEntity->Update(inDT);
+		theGameObject->Update(inDT);
 	}
 }
