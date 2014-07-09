e1a4f853d05b1ce5755cd5dc3498e6a082aff4ba
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-07-09 14:20:41 +0000

    Added small comments n' such

--- trunk/BreakoutClone/OpenGL Attempt1/EventMessenger.h
+++ trunk/BreakoutClone/OpenGL Attempt1/EventMessenger.h
@@ -11,11 +11,13 @@ enum EventTypes
 
 // Have things able to just #include this file and then subscribe themselves to specific events
 // They supply a function pointer to a function that the event messaging system will call when an event of that type shows up
-// Will likely declare this function pointer as a virtual function in either Gameobject or Component, though any file
+// Will likely declare this function pointer as a virtual function in either GameObject or Component, though any file
 // can subscribe to any event and provide their own custom function pointer
 
-// Actually will likely not put it in gameObject or Component, better to keep it only in the classes that need it
+// Actually will likely not put it in GameObject or Component, better to keep it only in the classes that need it
 // Maybe make an interface to inherit from that provides a couple of methods for receiving events and subscribing to them
+
+// Could also put in delayed events with an update method
 
 class IEventMessenger
 {
@@ -24,9 +26,11 @@ public:
 	void SendMessage(std::string inMessage) = 0;
 
 	// Loop through list of events and call function pointers for ones that match the recorded event - possibly 2d vector?
-	void RecordEvent(EventTypes inEventType);
+	void RecordEvent(EventTypes inEventType, float inEventNotificationDelay = 0.f);
 
 	static SubscribeToEvent(EventTypes inEventType, FunctionPointer inFunctionPointer);
+
+	void Update(float inDT);
 
 private:
 	std::vector<std::vector<EventType or something>> mEvents;
