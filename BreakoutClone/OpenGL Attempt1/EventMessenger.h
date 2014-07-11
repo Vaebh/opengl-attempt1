6a8435540c089e41d854800988ba302ad553021a
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-07-11 12:41:02 +0000

    Added some thoughts on how to do the EventMessanger

--- BreakoutClone/OpenGL Attempt1/EventMessenger.h
+++ BreakoutClone/OpenGL Attempt1/EventMessenger.h
@@ -3,7 +3,47 @@
 
 #include "Component.h"
 
+// Hold a vector/map of pairs, which each hold a string event id, and a vector of function pointers
+// This approach hinges on being able to compare function pointers. So when we subscribe to an event we
+// just check the event against the list of event id's and add the supplied function pointer to it's paired vector of function pointers
+
+/* e.g.
+vector<pair(string eventID, vector<functionPointers>)> eventList
+for(eventList)
+{
+	if(suppliedEventID == eventList[i].first)
+	{
+		eventList[i].second.push_back(suppliedFunctionPointer);
+	}
+}
+*/
+
+/* And when you unsubscribe you just check through the eventList for your matching eventID and function pointer
+/* e.g.
+vector<pair(string eventID, vector<functionPointers>)> eventList
+for(eventList)
+{
+	if(suppliedEventID == eventList[i].first)
+	{
+		for(functionPointerList)
+		{
+			if(suppliedFunctionPointer == functionPointerList[j])
+			{
+				functionPointerList[j].remove();
+			}
+		}
+	}
+}
+*/
+
+// Read in the string eventList from a file, make sure to have a SetEventListFile method so we can
+// set which file is on a per application basis.
+
+
+
 // Iffy on whether to use an enum, kind of want to just use raw strings so the messages can be more variable
+// Maybe just hold a list of pairs comprised of listeners and what events they're subscribed to and invoke them if the
+// received message matches their subscirbed one
 enum EventTypes
 {
 	PLAYER_MOVE,
