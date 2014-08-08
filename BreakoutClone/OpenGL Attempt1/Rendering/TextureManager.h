cd0e995a79f87d646e4065462888c65c6111043c
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-08-08 08:38:55 +0000

    Added texture manager, mostly empty header

---
+++ BreakoutClone/OpenGL Attempt1/Rendering/TextureManager.h
@@ -0,0 +1,20 @@
+//
+//  TextureManager.h
+//  OpenGL Attempt1
+//
+//  Created by Simon McDonnell on 07/08/2014.
+//  Copyright (c) 2014 ___SimonMcDonnell___. All rights reserved.
+//
+
+#ifndef TEXTUREMANAGER_SIMEGINE
+#define TEXTUREMANAGER_SIMEGINE
+
+class TextureManager
+{
+    TextureManager();
+    ~TextureManager();
+    
+    
+};
+
+#endif
