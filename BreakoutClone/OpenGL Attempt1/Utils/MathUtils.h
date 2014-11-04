b50305833db5d30bcc3e6a92b6fd902641555a86
simjmcd@gmail.com <simjmcd@gmail.com@1c697a90-d7f0-8125-287d-acccc1afde41>
2014-11-04 17:44:44 +0000

    Added a mathutils class

---
+++ BreakoutClone/OpenGL Attempt1/Utils/MathUtils.h
@@ -0,0 +1,10 @@
+#ifndef MATH_UTILS_SIMENGINE
+#define MATH_UTILS_SIMENGINE
+
+float ConvertToRange(float inOldMin, float inOldMax, float inNewMin, float inNewMax, float inValue)
+{
+	// Do the conversion stuff here
+	return inValue;
+}
+
+#endif
